e24e5c61d16118aebd67ba9483e38e9c8c1f3578
Berk Coşkun Mermer <mermerberkx@gmail.com>
2026-09-14 10:28:02 +0300

    Replace watermarked map tiles and refresh demo media

--- frontend/src/components/LiveMap.jsx
+++ frontend/src/components/LiveMap.jsx
@@ -103,8 +103,8 @@ const LiveMap = ({
     <div className="relative h-full w-full">
       <MapContainer center={center} zoom={13} style={{ height: '100%', width: '100%' }} zoomControl={false}>
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-          attribution='&copy; OpenStreetMap &copy; CARTO'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         />
 
         {pickupPos && (
